811e9f105faf685f84000314e655265b8adcf509
driewtonmai <bigbluelamp@gmail.com>
2019-11-13 18:09:27 +0600

    The commit will add test_views.py

--- courses/tests/test_views.py
+++ courses/tests/test_views.py
@@ -1,6 +1,40 @@
-# from django.test import TestCase
-#
-# from courses.models import *
-# from django.urls import reverse
-#
-# class CourseCreateViewTest(TestCase):
+import json
+from django.test import TestCase
+from courses.models import *
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class CourseTest(TestCase):
+    def setUp(self):
+        self.courses_data = {
+                                "name": "english zone",
+                                "description": "upgrade ur english bro!",
+                                "category": 1,
+                                "contacts": [
+                                    {
+                                        "value": "0551134155"
+                                    }
+                                ],
+                                "branches": [
+                                    {
+                                        "latitude": "145675567",
+                                        "longitude": "345790754",
+                                        "addres": "ул. Анала д. Карнавала"
+                                    }
+                                ]
+                            }
+
+
+    def test_course_create_view(self):
+        response = self.client.get(reverse('courses:courses'), self.courses_data)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_course_retrieve_view(self):
+        course = self.courses_data
+        response = self.client.get(reverse('courses:retrieve', kwargs={"pk":1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, course)
+
--- courses/urls.py
+++ courses/urls.py
@@ -7,6 +7,6 @@ app_name = 'courses'
 
 
 urlpatterns = [
-    path('courses/', CourseCreateView.as_view()),
-    path('retrieve/<int:pk>/', views.CourseRetrieveView.as_view()),
+    path('courses/', CourseCreateView.as_view(), name='courses'),
+    path('retrieve/<int:pk>/', views.CourseRetrieveView.as_view(), name='retrieve'),
 ]
