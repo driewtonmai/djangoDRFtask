352c173e967a3f6c008861bdb0b7408b91f279ed
driewtonmai <bigbluelamp@gmail.com>
2019-11-16 15:46:01 +0600

    The commit will add some changes

--- courses/tests/test_views.py
+++ courses/tests/test_views.py
@@ -1,9 +1,6 @@
-import json
 from django.test import TestCase
 from courses.models import *
 from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient
 
 
 class CourseTest(TestCase):
@@ -32,9 +29,9 @@ class CourseTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-    def test_course_retrieve_view(self):
-        course = self.courses_data
-        response = self.client.get(reverse('courses:retrieve', kwargs={"pk":1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, course)
-
+    # def test_course_retrieve_view(self):
+    #     course = self.courses_data
+    #     response = self.client.get(reverse('courses:retrieve', kwargs={"pk": 1}))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, course)
+    #
