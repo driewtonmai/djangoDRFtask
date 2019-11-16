578e93f33e62cc4e5015d70971befde02d215248
driewtonmai <bigbluelamp@gmail.com>
2019-11-16 16:17:27 +0600

    The commit will decouple PostgreSQL

--- DjangoTaskAPI/settings.py
+++ DjangoTaskAPI/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'DjangoTaskAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
